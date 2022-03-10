7909d1982c1e4d68f0216dad533ea963b962ef7d
Christoffer Artmann <Artgaard@gmail.com>
2022-03-10 12:10:49 +0100

    Initial Commit.

---
+++ app/root.tsx
@@ -0,0 +1,44 @@
+import {
+  Links,
+  LinksFunction,
+  LiveReload,
+  Meta,
+  MetaFunction,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from 'remix'
+
+import globalStyles from './index.css'
+import styles from './tailwind.css'
+
+export const links: LinksFunction = () => [
+  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+  { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+  { rel: 'stylesheet', href: globalStyles },
+  { rel: 'stylesheet', href: styles },
+  { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap' }
+];
+
+export const meta: MetaFunction = () => {
+  return { title: 'New Remix App' }
+}
+
+export default function App() {
+  return (
+    <html lang='en'>
+      <head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width,initial-scale=1' />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  )
+}
