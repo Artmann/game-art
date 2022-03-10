585d87a88d65d1928c12e40688a6662d5099149c
Christoffer Artmann <Artgaard@gmail.com>
2022-03-10 22:04:23 +0100

    chore: Add migrations.

---
+++ prisma/migrations/20220310211000_add_users/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "hash" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT E'user',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
